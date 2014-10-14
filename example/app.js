fbaa94cbe7e87decc32e704d700e6f4eb076fbbb
Zongmin Lei <leizongmin@gmail.com>
2014-10-14 17:06:25 +0800

    example

---
+++ example/app.js
@@ -0,0 +1,21 @@
+/**
+ * OnePage Example
+ *
+ * @author Zongmin Lei <leizongmin@gmail.com>
+ */
+
+var express = require('express');
+var OnePage = require('../');
+
+var one = new OnePage();
+one.add('demo', __dirname + '/demo');
+
+var app = express();
+app.use(one.middleware);
+
+app.get('/', function (req, res, next) {
+  console.log('GET %s', req.url);
+  res.renderOnePage('demo');
+});
+
+app.listen(8080);
